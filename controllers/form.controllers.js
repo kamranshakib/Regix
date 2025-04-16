215050d098d0ebfd97db78e885623069e4c87951
Kamran Shakib <kamranshakib371@gmail.com>
2025-04-16 12:52:30 -0700

    make id for each student

--- controllers/form.controllers.js
+++ controllers/form.controllers.js
@@ -1,19 +1,53 @@
 const express = require('express');
-const FormDB = require('../models/form.model')
+const FormDB = require('../models/form.model');
 const ejs = require('ejs');
 const app = require('../app');
 
-
-
-const GetForm = async(req, res) => {
- res.render('form.ejs')
+const GetForm = async (req, res) => {
+  res.render('form.ejs');
 };
-const SetForm = async(req,res)=>{
-   const newStudent = await (req.body)
-    const newST = await new FormDB(req.body)
-    await newST.save()
-    res.render('GetForm.ejs', { newStudent });
-   
-}
-  
+
+const SetForm = async (req, res) => {
+  try {
+    const lastStudent = await FormDB.findOne()
+      .sort({ studentID: -1 })
+      .exec();
+
+    let nextID = 'P24000000';
+
+    if (lastStudent && lastStudent.studentID) {
+      const lastIDNumber = parseInt(
+        lastStudent.studentID.replace('P240', '')
+      );
+      nextID =
+        'P240' +
+        (lastIDNumber + 1)
+          .toString()
+          .padStart(4, '0');
+    }
+
+    const newStudent = {
+      ...req.body,
+      studentID: nextID,
+    };
+
+    const newST = new FormDB(newStudent);
+
+    await newST.save();
+
+    res.render('GetForm.ejs', {
+      newStudent,
+      studentID: nextID,
+    });
+  } catch (error) {
+    console.error('Error saving student:', error);
+
+    res
+      .status(500)
+      .send(
+        `Error occurred while saving the student: ${error.message}`
+      );
+  }
+};
+
 module.exports = { SetForm, GetForm };
--- models/form.model.js
+++ models/form.model.js
@@ -2,20 +2,21 @@ const mongoose = require('mongoose')
 
 
 const FormSchema = new mongoose.Schema({
-    name:{type: String},                // اسم شاگرد
-    Lname: {type:String},              // تخلص
-    ID: {type:Number},                // ایدی
-    Fname: {type:String},            // اسم پدر
-    GFname: {type:String},          // اسم پدرکلان
-    BirthDay: {type:Number},       // روز تولد
-    Graduation: {type:Number},    // سال فراغت
-    School: {type:String},       // مکتب
-    Province: {type:String},    // ولایت
-    ExamLang: {type:String},   // زبان امتحان
-    CenterName: {type:String},// اسم مرکز
-    ExamPlace: {type:String},// مکان امتحان
-    ExamTime: {type:String},//زمان امتحان
-})
+  name: { type: String }, // اسم شاگرد
+  Lname: { type: String }, // تخلص
+  ID: { type: Number }, // ایدی
+  Fname: { type: String }, // اسم پدر
+  GFname: { type: String }, // اسم پدرکلان
+  BirthDay: { type: Number }, // روز تولد
+  Graduation: { type: Number }, // سال فراغت
+  School: { type: String }, // مکتب
+  Province: { type: String }, // ولایت
+  ExamLang: { type: String }, // زبان امتحان
+  CenterName: { type: String }, // اسم مرکز
+  ExamPlace: { type: String }, // مکان امتحان
+  ExamTime: { type: String }, //زمان امتحان
+  studentID:{type: String}
+});
 
 module.exports = mongoose.model('Register-fomr-ST', FormSchema)
 
