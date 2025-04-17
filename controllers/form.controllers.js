581ea3305b0169a239bf1520c5997030e490b652
Kamran Shakib <kamranshakib371@gmail.com>
2025-04-17 04:01:22 -0700

    create barcode for each students

--- controllers/form.controllers.js
+++ controllers/form.controllers.js
@@ -2,17 +2,20 @@ const express = require('express');
 const FormDB = require('../models/form.model');
 const ejs = require('ejs');
 const app = require('../app');
+const QRCode = require('qrcode');
+const bwipjs = require('bwip-js');
+const path = require('path');
+const fs = require('fs');
 
 const GetForm = async (req, res) => {
   res.render('form.ejs');
-};
-
+};       
+ 
 const SetForm = async (req, res) => {
   try {
     const lastStudent = await FormDB.findOne()
       .sort({ studentID: -1 })
       .exec();
-
     let nextID = 'P24000000';
 
     if (lastStudent && lastStudent.studentID) {
@@ -32,20 +35,42 @@ const SetForm = async (req, res) => {
     };
 
     const newST = new FormDB(newStudent);
-
     await newST.save();
+
+ 
+    const barcodeText = ` آی‌دی: ${nextID} ` ;
+
+    const barcodeBuffer = await bwipjs.toBuffer({
+      bcid: 'code128', 
+      text: barcodeText, 
+      scale: 1,
+      height: 20,
+      // includetext: true,
+      textxalign: 'center',
+    });
+
+ 
+    const fileName = `barcode-${nextID}.png`;
+    const filePath = path.join(
+      __dirname,
+      '..',
+      'public',
+      'barcodes',
+      fileName
+    );
+    fs.writeFileSync(filePath, barcodeBuffer);
 
     res.render('GetForm.ejs', {
       newStudent,
-      studentID:nextID
+      studentID: nextID,
+      barcodeImage: `/barcodes/${fileName}`,
     });
   } catch (error) {
     console.error('Error saving student:', error);
-
     res
       .status(500)
       .send(
-        `Error occurred while saving the student: ${error.message}`
+        `خطا در ذخیره شاگرد: ${error.message}`
       );
   }
 };
